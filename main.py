5394a2a96a4d00e4e2392d56d9fa3a974fb3d400
Enes Algul <86858992+Phenomex@users.noreply.github.com>
2021-09-01 05:35:28 +0000

    preP

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def myfunc(n):
   return lambda i: i*n
 
-doubler = myfunc(2)
-tripler = myfunc(3)
+ikikati = myfunc(2)
+uckati = myfunc(3)
 dortkati = myfunc(4)
 beskati = myfunc(5)
 altikati =myfunc(6)
@@ -10,4 +10,4 @@ yedikati =myfunc(7)
 sekizkati =myfunc(8)
 dokuzkati =myfunc(9)
 val = 2 ** 2
-print("Doubled: " + str(doubler(val))  + "\nTripled: " + str(tripler(val)) +  " \nQuadrupled: " +str(dortkati(val)) + "\nQunitupled: " +str(beskati(val)) + "\nSextupled: " +str(altikati(val)) + "\nSeptupled: " +str(yedikati(val)) +   "\nOctupled: " +str(sekizkati(val)) + "\nNonupled: " +str(dokuzkati(val)))
+print("Doubled: " + str(ikikati(val))  + "\nTripled: " + str(uckati(val)) +  " \nQuadrupled: " +str(dortkati(val)) + "\nQunitupled: " +str(beskati(val)) + "\nSextupled: " +str(altikati(val)) + "\nSeptupled: " +str(yedikati(val)) +   "\nOctupled: " +str(sekizkati(val)) + "\nNonupled: " +str(dokuzkati(val)))
